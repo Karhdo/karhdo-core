ebc1a1b7611567aa0d7320d93f6243d5f0732cc9
khanhdt1109 <khanhdt@younetgroup.com>
2024-05-29 21:31:19 +0700

    chore: Release package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,6 @@ export * from '@nestjs/swagger';
 export * from '@nestjs/platform-express';
 
 export * from './types';
-export * from './utils';
 export * from './enums';
+export * from './utils';
 export * from './decorators';
