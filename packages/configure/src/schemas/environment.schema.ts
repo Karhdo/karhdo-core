3fd474a1d594db9ed8852a2045b6279796b5ea67
khanhdt1109 <khanhdt@younetgroup.com>
2024-06-03 23:02:31 +0700

    chore: Release package

--- packages/configure/src/schemas/environment.schema.ts
+++ packages/configure/src/schemas/environment.schema.ts
@@ -1,20 +1,28 @@
 import { Type, Expose, plainToInstance } from 'class-transformer';
 import { ValidateNested, IsOptional } from 'class-validator';
-import { NestedObjectTransform } from '@karhdo/nestjs-core';
+import { NestedObjectTransform, NestedObjectValidator } from '@karhdo/nestjs-core';
 
 import { AppConfig } from './app.schema';
 import { RabbitMQConfig } from './rabbitmq.schema';
+import { AxiosConfig, HttpConfig } from './http.schema';
 import { Database, DatabaseConfig } from './database.schema';
 
 export class EnvironmentConfig {
+  constructor(config?: Partial<EnvironmentConfig>) {
+    Object.assign(this, config);
+  }
+
   @Type(() => AppConfig)
   @Expose()
   @ValidateNested()
   public readonly app: AppConfig;
 
-  constructor(config?: Partial<EnvironmentConfig>) {
-    Object.assign(this, config);
-  }
+  @IsOptional()
+  @NestedObjectValidator(AxiosConfig)
+  @NestedObjectTransform(AxiosConfig, ({ value }) => {
+    return plainToInstance(HttpConfig, value);
+  })
+  public readonly http: HttpConfig;
 
   @Type(() => RabbitMQConfig)
   @Expose()
