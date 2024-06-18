6956ef78b7ffc082971810c31975e5928c50045d
Karhdo <dotrongkhanh.dev@gmail.com>
2024-06-18 21:26:27 +0700

    feat(database): Build method upsert and upsetMany for sql

--- packages/database/src/database.type.ts
+++ packages/database/src/database.type.ts
@@ -4,6 +4,7 @@ import { Model, ModelCtor } from 'sequelize-typescript';
 import { SQLOptions } from './database.interface';
 
 export type Constructor<T = any> = new (...args: any[]) => T;
+
 export type Table<T> = Constructor<T> & typeof Model;
 
 export type Filter<T> = WhereOptions<T>;
